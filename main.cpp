a274572b0c53aba18da3323b99ee194362febc46
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-04-28 19:36:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+ int lista[10], b, c, x, y, d, i;
+ for(int i=0;i<10;i++){
+ cin >> lista[i];
+ }
+ int a=0;
+ cin >> a;
+ 
+
+ for(i=0; i<10; i++)
+ {
+ 	if(a<=lista[i])
+ 	{
+ 		b=lista[i];
+	 	lista[i]=a;
+	 	for(i=i+1;i<10;i++)
+	 	{
+	 		c=lista[i];
+	 		x=c;
+	 		lista[i]=b;
+	 		b=x;
+	 		
+		}
+	}
+ }
+ 
+ for(int i=0;i<10;i++){
+ cout << lista[i] << " ";
+ 
+ }
+ return 0;
 }
